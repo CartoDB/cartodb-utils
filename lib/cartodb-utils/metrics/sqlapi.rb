48a54be2877f919d3169254a1fe4e659dc6e3d50
Antonio Zamorano <azamorano@cartodb.com>
2015-10-26 18:20:44 +0100

    Fixed typo with column name

--- lib/cartodb-utils/metrics/sqlapi.rb
+++ lib/cartodb-utils/metrics/sqlapi.rb
@@ -56,7 +56,7 @@ module CartoDBUtils
         AND service='##SERVICE##'
         RETURNING *
       )
-      INSERT INTO service_status (timestamp, cloud_name, host_group, host, service, service_status)
+      INSERT INTO service_status (timestamp, cloud_name, host_group, host, service, value)
         SELECT '##TIMESTAMP##', '##CLOUD_NAME##', '##HOST_GROUP##', '##HOST##', '##SERVICE##', ##SERVICE_STATUS##
         WHERE NOT EXISTS (SELECT * FROM upsert);
       EOS
